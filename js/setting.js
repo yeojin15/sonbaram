dc641e19b75316e8f58a035113e04a6e1638b568
yjkim <k.yeojin15@gmail.com>
2023-05-08 21:10:32 +0900

    230508 21:10 게시판상세까지

--- js/setting.js
+++ js/setting.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-  $('input[name="balance-month"]').on('change', function() {
-    if ($('#balanceMonthAll').is(':checked')) {
+  $('input[name="set-month"]').on('change', function() {
+    if ($('#setMonthAll').is(':checked')) {
       $('p.date input[type="date"]').prop('disabled', true);
-    } else if ($('#balanceMonthSelect').is(':checked')) {
+    } else if ($('#setMonthSelect').is(':checked')) {
       $('p.date input[type="date"]').prop('disabled', false);
     }
   });
