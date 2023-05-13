c48482284fc7b05007728a6282370bb91e2682da
yjkim <k.yeojin15@gmail.com>
2023-05-14 01:32:10 +0900

    230514 모바일버전 추가

--- js/common.js
+++ js/common.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  // aside 토글 액션
+  // pc header
   $('#HEADER_PC .toggle').click(function(){
     $('#HEADER_PC').toggleClass('toggleOn');
     $('#PAGE').toggleClass('toggleOn');
@@ -11,6 +11,24 @@ $(document).ready(function() {
     $('#SNB_PC').removeClass('toggleOn');
   })
 
+  // mo header
+  $('#HEADER_MO .toggle').click(function(){
+    $('#HEADER_MO').toggleClass('toggleOn');
+  });
+  $('.aside-back, .snb-info li, .snb-menu li').click(function(){
+    $('#HEADER_MO').removeClass('toggleOn');
+  })
+
+  // mo 제목 탭
+  $('.tab-list li').each(function(){
+    $(this).click(function(){
+      let idx = $(this).index();
+      $('.tab-list li').removeClass('active');
+      $(this).addClass('active');
+      $('.app-tab').removeClass('active');
+      $('.app-tab').eq(idx).addClass('active')
+    });
+  })
 
   $('input:not([type="radio"]):not([type="checkbox"])').each(function() {
     if($(this).attr('value')!=undefined) $(this).prop('disabled', true)
