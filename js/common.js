5fe95e5a5b5fd83363c69fa5c19197dc954a4a9c
yjkim <k.yeojin15@gmail.com>
2023-05-14 22:31:20 +0900

    230514 22:31 모바일버전, pc 새 페이지 추가

--- js/common.js
+++ js/common.js
@@ -30,7 +30,43 @@ $(document).ready(function() {
     });
   })
 
+  // modal close
+  $('.modalClose').click(function(){
+    $('.modal').removeClass('modalOn');
+  });
+
+  // popup close
+  $('.popClose').click(function(){
+    $('.popup').removeClass('popOpen');
+  })
+
+  // 팝업창 내 탭 선택
+  $('.pop-tablist li').each(function(){
+    $(this).click(function(){
+      let idx = $(this).index();
+      $('.pop-tablist li').removeClass('active');
+      $(this).addClass('active');
+      $('.pop-tab').removeClass('active');
+      $('.pop-tab').eq(idx).addClass('active')
+    });
+  })
+  
+  // 이미지업로드
+  $('.input-images').each(function(){
+    $(this).imageUploader();
+  })
+
+  // input disabled 처리
   $('input:not([type="radio"]):not([type="checkbox"])').each(function() {
     if($(this).attr('value')!=undefined) $(this).prop('disabled', true)
+  });
+
+  // 등록일 설정 체크 시 input date abled
+  $('input[name="set-month"]').on('change', function() {
+    if ($('#setMonthAll').is(':checked')) {
+      $('p.date input[type="date"]').prop('disabled', true);
+    } else if ($('#setMonthSelect').is(':checked')) {
+      $('p.date input[type="date"]').prop('disabled', false);
+    }
   });
 });
